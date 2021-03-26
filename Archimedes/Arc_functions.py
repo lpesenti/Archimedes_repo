b264f431548f9919e6c6ecf019d424e11d31fbba
lpesenti <l.pesenti6@campus.unimib.it>
2021-03-26 12:52:42 +0100

    First comments added

--- Archimedes/Arc_functions.py
+++ Archimedes/Arc_functions.py
@@ -6,14 +6,50 @@ import os
 import glob
 import re
 
-unix_time = 0
+unix_time = 0  # It is used only to make right conversion in time for time evolution analysis
 path_to_data = r"C:\Users\lpese\PycharmProjects\Archimedes-Sassari\Archimedes\Data"
 freq = 1000  # Hz
 
 
 def read_data(day, month, year, col_to_save, num_d):
+    """
+    Search data present in a specific folder and read only the column associated with the quantity you are interested in
+
+    Parameters
+    ----------
+    day : int
+        It refers to the first day of the data to be read
+
+    month : int
+        It refers to the first month of the data to be read
+
+    year : int
+        It refers to the first year of the data to be read
+
+    col_to_save : str
+        The quantity to be read. It must be one of the following
+
+        - 'ITF' : the signal from the interferometer expressed in V
+        - 'Pick Off' : the signal from the pick-off expressed in V
+        - 'Signal injected' : the signal from the waveform used expressed in V
+        - 'Error' :
+        - 'Correction' :
+        - 'Actuator 1' : the output of the actuator 1 before amplification expressed in V
+        - 'Actuator 2' : the output of the actuator 2 before amplification expressed in V
+        - 'After Noise' :
+        - 'Time' : the timestamp of the data saved every milli second in human-readable format
+
+    num_d : int
+        How many days of data you want to analyze.
+
+    Returns
+    -------
+    A tuple of a pandas DataFrame [n-rows x 1-column] containing the data, the index of the column and the timestamp
+    expressed in UNIX
+    """
+
     print('--------------- Reading', col_to_save, 'data ---------------')
-    month = '%02d' % month  # It transforms 2 -> 02
+    month = '%02d' % month  # It transforms 1,2,3,... -> 01,02,03,...
     cols = np.array(
         ['ITF', 'Pick Off', 'Signal injected', 'Error', 'Correction', 'Actuator 1', 'Actuator 2', 'After Noise',
          'Time'])
@@ -38,7 +74,7 @@ def read_data(day, month, year, col_to_save, num_d):
     return final_df, index, timestamp
 
 
-def log_tick_formatter(val, pos=None):
+def time_tick_formatter(val, pos=None):
     global unix_time
     if val % 1000 == 0:
         val = str(datetime.datetime.fromtimestamp(int(unix_time + val * 0.001)))
@@ -57,7 +93,7 @@ def time_evolution(day, month, year, quantity, ax, ndays=1):
     ax.plot(df.index, df[col_index], label=lab)
     ax.grid(True, linestyle='-')
     ax.set_ylabel('Voltage [V]')
-    ax.xaxis.set_major_formatter(log_tick_formatter)
+    ax.xaxis.set_major_formatter(time_tick_formatter)
     ax.legend(loc='best', shadow=True, fontsize='medium')
     # ax.xaxis.set_tick_params(rotation=30)
     return ax, filename
