5da8bd3047a6ca211b8e2c5a3484796279369368
lpesenti <l.pesenti6@campus.unimib.it>
2021-04-19 17:04:58 +0200

    Solved data format in time evolution plot and similar

--- Archimedes/Arc_functions.py
+++ Archimedes/Arc_functions.py
@@ -27,6 +27,7 @@ In the current configuration the sampling rate is 1 KHz but can be increase.
 
 from matplotlib.mlab import cohere
 from matplotlib import mlab
+import matplotlib.dates as mdates
 from itertools import groupby
 import datetime
 import numpy as np
@@ -101,7 +102,18 @@ def time_tick_formatter(val, pos=None):
     return val
 
 
-def read_data(day, month, year, col_to_save, num_d, file_start=None, file_stop=None, verbose=True):
+def myfromtimestampfunction(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp)
+
+
+def myvectorizer(input_func):
+    def output_func(array_of_numbers):
+        return [input_func(a) for a in array_of_numbers]
+
+    return output_func
+
+
+def read_data(day, month, year, col_to_save, num_d=1, file_start=None, file_stop=None, verbose=True):
     """
     Search data present in a specific folder and read only the column associated with the quantity you are interested in
 
@@ -158,14 +170,12 @@ def read_data(day, month, year, col_to_save, num_d, file_start=None, file_stop=N
     index = np.where(cols == col_to_save)[0][0] + 1  # Find the index corresponding to the the col_to_save
     all_data = []
     final_df = pd.DataFrame()
+    time_array = []
     for i in range(num_d):  # Loop over number of days
         data_folder = os.path.join(path_to_data, "SosEnattos_Data_{0}{1}{2}".format(year, month, day + i))
         temp_data = glob.glob(os.path.join(data_folder, "*.lvm"))
         temp_data.sort(key=lambda f: int(re.sub('\D', '', f)))
         all_data += temp_data
-    time = pd.read_table(all_data[0], sep='\t', usecols=[9], header=None)  # Read the time from first data
-    start_t = time[9][0].replace("\\", '')  # Remove \\ in the data format -> it allows to be converted
-    timestamp = datetime.datetime.timestamp(pd.to_datetime(start_t))  # Conversion of the time to timestamp
     i = 1
     logging.debug('Number of *.lvm: %i' % len(all_data))
     if file_start and file_stop:
@@ -177,6 +187,11 @@ def read_data(day, month, year, col_to_save, num_d, file_start=None, file_stop=N
                 a = pd.read_table(data, sep='\t', usecols=[index], header=None)
                 # At the end we have a long column with all data
                 final_df = pd.concat([final_df, a], axis=0, ignore_index=True)
+                time = pd.read_table(data, sep='\t', usecols=[9], header=None).replace(r'\\09', '',
+                                                                                       regex=True).values.flatten().tolist()
+                timestamp = datetime.datetime.timestamp(pd.to_datetime(time[0]))
+                for j in range(1, len(time) + 1):
+                    time_array.append(timestamp + j / freq)
             i += 1
     elif file_start and not file_stop:
         for data in all_data:
@@ -187,6 +202,11 @@ def read_data(day, month, year, col_to_save, num_d, file_start=None, file_stop=N
                 a = pd.read_table(data, sep='\t', usecols=[index], header=None)
                 # At the end we have a long column with all data
                 final_df = pd.concat([final_df, a], axis=0, ignore_index=True)
+                time = pd.read_table(data, sep='\t', usecols=[9], header=None).replace(r'\\09', '',
+                                                                                       regex=True).values.flatten().tolist()
+                timestamp = datetime.datetime.timestamp(pd.to_datetime(time[0]))
+                for j in range(1, len(time) + 1):
+                    time_array.append(timestamp + j / freq)
             i += 1
     elif file_stop and not file_start:
         for data in all_data:
@@ -197,6 +217,11 @@ def read_data(day, month, year, col_to_save, num_d, file_start=None, file_stop=N
                 a = pd.read_table(data, sep='\t', usecols=[index], header=None)
                 # At the end we have a long column with all data
                 final_df = pd.concat([final_df, a], axis=0, ignore_index=True)
+                time = pd.read_table(data, sep='\t', usecols=[9], header=None).replace(r'\\09', '',
+                                                                                       regex=True).values.flatten().tolist()
+                timestamp = datetime.datetime.timestamp(pd.to_datetime(time[0]))
+                for j in range(1, len(time) + 1):
+                    time_array.append(timestamp + j / freq)
             i += 1
     else:
         for data in all_data:
@@ -206,14 +231,18 @@ def read_data(day, month, year, col_to_save, num_d, file_start=None, file_stop=N
             a = pd.read_table(data, sep='\t', usecols=[index], header=None)
             # At the end we have a long column with all data
             final_df = pd.concat([final_df, a], axis=0, ignore_index=True)
-            i += 1
+            time = pd.read_table(data, sep='\t', usecols=[9], header=None).replace(r'\\09', '',
+                                                                                   regex=True).values.flatten().tolist()
+            timestamp = datetime.datetime.timestamp(pd.to_datetime(time[0]))
+            for j in range(1, len(time) + 1):
+                time_array.append(timestamp + j / freq)
     if verbose:
         print('--------------- Reading completed! ---------------')
     logging.info('Data read completed')
-    return final_df, index, timestamp
+    return final_df, index, time_array
 
 
-def time_evolution(day, month, year, quantity, ax, ndays=1, verbose=True):
+def time_evolution(day, month, year, quantity, ax, ndays=1, file_start=None, file_stop=None, verbose=True):
     """
     Make the plot of time evolution
 
@@ -254,17 +283,27 @@ def time_evolution(day, month, year, quantity, ax, ndays=1, verbose=True):
     -------
     A tuple of an axes and the relative filename
     """
-    df, col_index, t = read_data(day, month, year, quantity, ndays, verbose=verbose)
-    global unix_time  # Access the global variable and
-    unix_time = t  # change its value to the timestamp of the data
+    df, col_index, t = read_data(day, month, year, quantity, ndays, file_start=file_start, file_stop=file_stop,
+                                 verbose=verbose)
+    # global unix_time  # Access the global variable and
+    # unix_time = t  # change its value to the timestamp of the data
     if verbose:
         print('Building Time Evolution Plot...')
+    date_convert = myvectorizer(myfromtimestampfunction)
+    xtimex = date_convert(t)
+    datenums = mdates.date2num(xtimex)
     lab = quantity + ' (n° days: ' + str(ndays) + ')'
     filename = str(year) + str(month) + str(day) + '_' + quantity + '_nDays_' + str(ndays) + 'tEvo'
-    ax.plot(df.index, df[col_index], color='tab:red', label=lab)
+    ax.plot(datenums, df[col_index], color='tab:red', label=lab)
     ax.grid(True, linestyle='-')
     ax.set_ylabel('Voltage [V]')
-    ax.xaxis.set_major_formatter(time_tick_formatter)
+    xfmt = mdates.DateFormatter('%b %d %H:%M:%S')
+    ax.xaxis.set_major_formatter(xfmt)
+    # ax.xaxis.set_major_formatter(time_tick_formatter)
+    # print(pd.to_datetime(t[0]))
+    # ax.set_xticks(np.arange(0, df.size, int(df.size / len(t))))
+    # ax.set_xticklabels(t)
+    # ax.xaxis.set_minor_locator(AutoMinorLocator())
     ax.legend(loc='best', shadow=True, fontsize='medium')
     return ax, filename
 
@@ -358,18 +397,21 @@ def th_comparison(data_frame, threshold=0.03, length=10000, verbose=True):
     return data_to_check, frac_rejected
 
 
-def psd(day, month, year, quantity, ax, interval, mode, low_freq=2, high_freq=10, threshold=0.03, ndays=1, length=10000,
-        file_start=None, file_stop=None, verbose=True):
+def psd(day, month, year, quantity, ax, interval, mode, low_freq=2, high_freq=10, threshold=0.03, ndays=1,
+        length=10000, ax1=None, file_start=None, file_stop=None, verbose=True):
     logging.info('PSD evaluator started')
     logging.debug(
         'PARAMETERS: day=%i month=%i year=%i quantity=%s ax=%s interval=%i'
         ' mode=%s low_freq=%i high_freq=%i threshold=%f ndays=%i length=%i verbose=%s' % (
             day, month, year, quantity, ax, interval, mode, low_freq, high_freq, threshold, ndays, length, verbose))
 
-    df_qty, _, _ = read_data(day, month, year, quantity, ndays, file_start=file_start, file_stop=file_stop,
-                             verbose=verbose)
-    df_po, _, _ = read_data(day=day, month=month, year=year, col_to_save='Pick Off', num_d=ndays, file_start=file_start,
-                            file_stop=file_stop, verbose=verbose)
+    df_qty, col_index, t = read_data(day, month, year, quantity, num_d=ndays, file_start=file_start,
+                                     file_stop=file_stop,
+                                     verbose=verbose)
+    df_itf, _, _ = read_data(day=day, month=month, year=year, col_to_save='ITF', num_d=ndays, file_start=None,
+                             file_stop=None, verbose=verbose)
+    df_po, _, _ = read_data(day=day, month=month, year=year, col_to_save='Pick Off', num_d=ndays, file_start=None,
+                            file_stop=None, verbose=verbose)
 
     data_cleared, _ = th_comparison(df_qty, threshold=threshold, length=length, verbose=verbose)
 
@@ -380,16 +422,20 @@ def psd(day, month, year, quantity, ax, interval, mode, low_freq=2, high_freq=10
     logging.debug('Data cleared from NaN values')
 
     num = int(60 * freq)
-    _, psd_f = mlab.psd(np.ones(num), NFFT=num, Fs=freq, detrend="linear", noverlap=int(num / 2))
+    _, psd_f = mlab.psd(np.ones(num), NFFT=num, Fs=freq, detrend="linear")  # , noverlap=int(num / 2))
     psd_f = psd_f[1:]
 
     outdata, file_index = [], []
-    len_max = 0
+    len_max, pick_off_mean = 0, 0
+    length_data_used = 0
     integral_min = np.inf
+
     start = np.where(psd_f == low_freq)[0][0]
     stop = np.where(psd_f == high_freq)[0][0]
+
     print('Number of usable array:', len(data_first_check)) if verbose else print()
     print('Starting evaluation of', mode, 'PSD...') if verbose else print()
+
     if mode == 'max interval':
         for el in data_first_check:
             if len(el) >= interval * freq and len(el) > len_max:
@@ -402,13 +448,17 @@ def psd(day, month, year, quantity, ax, interval, mode, low_freq=2, high_freq=10
         for el in data_first_check:
             print(round(i / len(data_first_check) * 100, 2), '%')
             if len(el) >= interval * freq:
-                el_s, _ = mlab.psd(el, NFFT=num, Fs=freq, detrend="linear", noverlap=int(num / 2))
+                el_s, _ = mlab.psd(el, NFFT=num, Fs=freq, detrend="linear")  # , noverlap=int(num / 2))
                 el_s = el_s[1:]
-                integral = sum(el_s[start:stop] / num)  # Width bin = 1/num
+                integral = sum(
+                    el_s[start:stop] / len(el_s[start:stop]))  # len(el) differential in the integral to evaluate RMS
                 if integral < integral_min:
                     integral_min = integral
-                    file_index = list(find_rk(data_cleared, el))
+                    file_index = list(find_rk(df_qty.values.flatten(), el))
                     outdata = el_s
+                    length_data_used = len(el)
+                    pick_off_mean = np.abs(
+                        np.mean(df_po[file_index[0]:file_index[0] + len(el) + 1].values.flatten()))
             i += 1
         if file_index:
             file_number = file_index[0] / 300000
@@ -416,25 +466,42 @@ def psd(day, month, year, quantity, ax, interval, mode, low_freq=2, high_freq=10
                                                                                         'and',
                                                                                         int(file_number) + 1)
     print('Evaluation of', mode, 'PSD completed!') if verbose else print()
-    print('Length of data used for PSD:', len(outdata)) if verbose else print()
-    v_max = df_qty.max().values.flatten()
-    v_min = df_qty.min().values.flatten()
+    # print('Length of data used for PSD:', len(outdata)) if verbose else print()
+    v_max = df_itf.max().values.flatten()
+    v_min = df_itf.min().values.flatten()
+    print('Voltage max', v_max)
+    print('Voltage min', v_min)
     alpha = first_coef / (v_max - v_min)
-
-    pick_off_mean = np.abs(np.mean(df_po[file_index[0]:file_index[0]+len(outdata) + 1].values.flatten()))
+    print('Alpha', alpha)
+    logging.info('V_max = %f, V_min = %f, alpha = %f' % (v_max, v_min, alpha))
+    # if outdata:
     data_to_plot = np.sqrt(outdata) * alpha * pick_off_mean
+    # else:
+    #    data_to_plot = np.empty(len(psd_f)) * np.nan
     lab1 = r'$\sqrt{PSD}$ of ' + quantity + '(' + str(interval) + ' s)'
 
     x, y = np.loadtxt(os.path.join(path_to_data, 'VirgoData_Jul2019.txt'), unpack=True, usecols=[0, 1])
+    data_used_x = df_qty.index[file_index[0]:file_index[0] + length_data_used + 1]
+    data_used_y = df_qty[file_index[0]:file_index[0] + length_data_used + 1].values.flatten()
     ax.plot(x, y, linestyle='-', label='@ Virgo')
     ax.plot(psd_f, data_to_plot, linestyle='-', label='@ Sos-Enattos')
-    ax.set_xscale("log")
+    ax.set_xscale("linear")
+    ax.set_xlim([2, 20])
+    ax.set_ylim([1.e-13, 1.e-8])
     ax.set_yscale("log")
     ax.set_xlabel("Frequency (Hz)", fontsize=16)
     ax.set_ylabel(r"ASD [rad/$\sqrt{Hz}$]", fontsize=16)
     ax.grid(True, linestyle='-')
     ax.legend(loc='best', shadow=True, fontsize='medium')
-    return ax
+    ax.set_title(mode)
+    if ax1:
+        ax1.plot(df_qty.index, df_qty[col_index], linestyle='dotted', label='All data')
+        ax1.plot(data_used_x, data_used_y, linestyle='-', label='Data used')
+        ax1.grid(True, linestyle='-')
+        # ax1.xaxis.set_major_formatter(time_tick_formatter)
+        ax1.set_ylabel(r"Voltage [V]", fontsize=16)
+        ax1.legend(loc='best', shadow=True, fontsize='medium')
+    return ax, ax1
 
 
 def cleared_plot(day, month, year, quantity, ax, threshold=0.03, ndays=1, length=10000, verbose=True):
@@ -491,7 +558,7 @@ def cleared_plot(day, month, year, quantity, ax, threshold=0.03, ndays=1, length
     -------
     A tuple of an axes and the relative filename
     """
-    df, _, t = read_data(day, month, year, quantity, ndays, verbose=verbose)
+    df, col_index, t = read_data(day, month, year, quantity, ndays, verbose=verbose)
     global unix_time
     unix_time = t
     data_und_th, val_rej = th_comparison(df, threshold=threshold, length=length, verbose=verbose)
@@ -503,7 +570,8 @@ def cleared_plot(day, month, year, quantity, ax, threshold=0.03, ndays=1, length
         print('--------------- Building Derivative and Data_Cleared Plot! ---------------')
     # ax.plot(df.index, data_deriv, color='tab:green', linestyle='-', label=lab)
     # ax.plot(np.arange(data_deriv.size), th_line, color='tab:red', linestyle='-', label='threshold')
-    ax.plot(df.index, data_und_th + 5, color='tab:blue', linestyle='-', label=lab1)
+    ax.plot(df.index, df[col_index], color='tab:red', label='Data')
+    ax.plot(df.index, data_und_th + 5, color='tab:blue', linestyle='-', label='Under threshold')
     ax.grid(True, linestyle='-')
     ax.xaxis.set_major_formatter(time_tick_formatter)
     ax.legend(loc='best', shadow=True, fontsize='medium')
