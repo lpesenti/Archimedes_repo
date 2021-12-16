725e77f135ee29ee4a56b1eaf16aa3420845f095
lpesenti <l.pesenti6@campus.unimib.it>
2021-12-16 19:39:39 +0100

    Added ratios functions and difference one.

--- Optical_Lever/OL_noise.py
+++ Optical_Lever/OL_noise.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import matplotlib.gridspec as gridspec
+from win32com.makegw.makegwparse import error_not_supported
 
 
 def old():
@@ -235,16 +236,51 @@ def old():
 
 def asd_extractor(file_directory, filename, samp_freq, psd_window, channel, means_number=1, final_df=pd.DataFrame(),
                   offset=1,
-                  max_length=1200000):
+                  max_length=600):
+    """
+
+    :param file_directory: The directory where the file is stored
+    :type file_directory: str
+
+    :param filename: The name of the file
+    :type filename: str
+
+    :param samp_freq: The sampling frequency used
+    :type samp_freq: int
+
+    :param psd_window: Represents the length, in seconds, of the psd
+    :type psd_window: int
+
+    :param channel: One of the three channel acquired by the OL-shield. It MUST BE one of the following: Dy, Dx or Sum
+    :type channel: str
+
+    :param means_number: Specify how many means for each psd evaluation you want to perform
+    :type means_number: int
+
+    :param final_df: Already existing DataFrame to which you want to append the new data
+    :type final_df: pandas.DataFrame
+
+    :param offset: If you want to apply a vertical translation to the data
+    :type offset: float
+
+    :param max_length: Specify the maximum length of the data, in seconds, to be used. It is used to uniform the length
+     between the channels
+    :type max_length: int
+
+    :return: A tuple of: a DataFrame containing three columns (asd_data, freq_data and Channels) the ratio between the
+     psd window and the means used (necessary to evaluate the minimum frequency achieved)
+     and the sample frequency applied
+    """
     # TODO: Add function description
     Num = samp_freq * psd_window
+    last_index = max_length * samp_freq
     df = pd.read_table(os.path.join(file_directory, filename),
                        sep='\t',
                        usecols=[0, 1, 2, 3],
                        header=None,
                        names=['NaN', 'Dy', 'Dx', 'Sum'])
     vec_asd, freq_asd = np.array([]), np.array([])
-    data = np.array_split(df[channel][:max_length].values.flatten() * offset, max_length / Num)
+    data = np.array_split(df[channel][:last_index].values.flatten() * offset, last_index / Num)
     nfft_used = int(Num / means_number)
     for i in range(len(data)):
         psd, freq = mlab.psd(data[i], NFFT=nfft_used, Fs=samp_freq, detrend="linear")  # ,noverlap=NOL)
@@ -262,13 +298,137 @@ def asd_extractor(file_directory, filename, samp_freq, psd_window, channel, mean
     return final_df, psd_window / means_number, samp_freq  # freq_asd, vec_asd, final_df
 
 
-def ratio(first_df, second_df):
+def ratio(first_df, second_df, lower_quantile, upper_quantile, estimator='mean'):
     # TODO: Add function description
-    ch = np.array('{0}/{1}'.format(first_df['Channels'][0], second_df['Channels'][0])).repeat(len(first_df['Channels']))
-    ratio_df = pd.DataFrame(
-        {'ratio': first_df['asd_data'] / second_df['asd_data'], 'freq_data': first_df['freq_data'], 'Channels': ch},
-        columns=['ratio', 'freq_data', 'Channels'])
-    return ratio_df
+    try:
+        if estimator.lower() == 'mean':
+            first_estim = first_df.groupby(['freq_data'])['asd_data'].mean()
+            second_estim = second_df.groupby(['freq_data'])['asd_data'].mean()
+        elif estimator.lower() == 'median':
+            first_estim = first_df.groupby(['freq_data'])['asd_data'].median()
+            second_estim = second_df.groupby(['freq_data'])['asd_data'].median()
+        else:
+            raise ValueError('The estimator must be one between median or mean')
+    except ValueError:
+        raise
+
+    try:
+        if not 0 <= lower_quantile and upper_quantile <= 1:
+            raise ValueError('The quantile must have a value between 0 and 1')
+    except ValueError:
+        raise
+
+    # Find the quantile value grouping by frequency data
+    first_low_quantile = first_df.groupby(['freq_data'])['asd_data'].quantile(lower_quantile)
+    first_up_quantile = first_df.groupby(['freq_data'])['asd_data'].quantile(upper_quantile)
+    second_low_quantile = second_df.groupby(['freq_data'])['asd_data'].quantile(lower_quantile)
+    second_up_quantile = second_df.groupby(['freq_data'])['asd_data'].quantile(upper_quantile)
+
+    # Evaluate the ratio between the quantities
+    ratio_estim = first_estim / second_estim
+    ratio_low_quantile = first_low_quantile / second_low_quantile
+    ratio_up_quantile = first_up_quantile / second_up_quantile
+
+    # Frequencies are the indeces of the DataFrame because of groupby()
+    frequency_array = ratio_estim.index.to_numpy()
+
+    # Convert the DataFrame in numpy array
+    ratio_estim_array = ratio_estim.to_numpy()
+    ratio_low_quantile_array = ratio_low_quantile.to_numpy()
+    ratio_up_quantile_array = ratio_up_quantile.to_numpy()
+
+    label_1 = first_df['Channels'][0].split(' ')[1].replace('(', '').replace(')', '')
+    label_2 = second_df['Channels'][0].split(' ')[1].replace('(', '').replace(')', '')
+    ratio_label = label_1 + '/' + label_2
+
+    return frequency_array, ratio_estim_array, ratio_low_quantile_array, ratio_up_quantile_array, ratio_label
+
+
+def ratio_std(first_df, second_df, num_std=1, estimator='mean'):
+    # TODO: Add function description
+    # TODO: Create mean variable as default because in ratio it is performed (mean1 +- n*std)/(mean2 +- n*std)
+    try:
+        if estimator.lower() == 'mean':
+            first_estim = first_df.groupby(['freq_data'])['asd_data'].mean()
+            second_estim = second_df.groupby(['freq_data'])['asd_data'].mean()
+        elif estimator.lower() == 'median':
+            first_estim = first_df.groupby(['freq_data'])['asd_data'].median()
+            second_estim = second_df.groupby(['freq_data'])['asd_data'].median()
+        else:
+            raise ValueError('The estimator must be one between median or mean')
+    except ValueError:
+        raise
+
+    # Find the std value grouping by frequency data
+    first_std = first_df.groupby(['freq_data'])['asd_data'].std()
+    second_std = second_df.groupby(['freq_data'])['asd_data'].std()
+
+    # Evaluate the ratio between the quantities
+    std_estim = first_estim / second_estim
+    std_low_quantile = (first_df.groupby(['freq_data'])['asd_data'].mean() + num_std * first_std) / (
+                second_df.groupby(['freq_data'])['asd_data'].mean() + num_std * second_std)
+    std_up_quantile = (first_df.groupby(['freq_data'])['asd_data'].mean() - num_std * first_std) / (
+                second_df.groupby(['freq_data'])['asd_data'].mean() - num_std * second_std)
+
+    # Frequencies are the indeces of the DataFrame because of groupby()
+    frequency_array = std_estim.index.to_numpy()
+
+    # Convert the DataFrame in numpy array
+    std_estim_array = std_estim.to_numpy()
+    std_low_quantile_array = std_low_quantile.to_numpy()
+    std_up_quantile_array = std_up_quantile.to_numpy()
+
+    label_1 = first_df['Channels'][0].split(' ')[1].replace('(', '').replace(')', '')
+    label_2 = second_df['Channels'][0].split(' ')[1].replace('(', '').replace(')', '')
+    ratio_label = label_1 + '/' + label_2
+
+    return frequency_array, std_estim_array, std_low_quantile_array, std_up_quantile_array, ratio_label
+
+
+def difference(first_df, second_df, lower_quantile, upper_quantile, estimator='mean'):
+    # TODO: Add function description
+    try:
+        if estimator.lower() == 'mean':
+            first_estim = first_df.groupby(['freq_data'])['asd_data'].mean()
+            second_estim = second_df.groupby(['freq_data'])['asd_data'].mean()
+        elif estimator.lower() == 'median':
+            first_estim = first_df.groupby(['freq_data'])['asd_data'].median()
+            second_estim = second_df.groupby(['freq_data'])['asd_data'].median()
+        else:
+            raise ValueError('The estimator must be one between median or mean')
+    except ValueError:
+        raise
+
+    try:
+        if not 0 <= lower_quantile and upper_quantile <= 1:
+            raise ValueError('The quantile must have a value between 0 and 1')
+    except ValueError:
+        raise
+
+    # Find the quantile value grouping by frequency data
+    first_low_quantile = first_df.groupby(['freq_data'])['asd_data'].quantile(lower_quantile)
+    first_up_quantile = first_df.groupby(['freq_data'])['asd_data'].quantile(upper_quantile)
+    second_low_quantile = second_df.groupby(['freq_data'])['asd_data'].quantile(lower_quantile)
+    second_up_quantile = second_df.groupby(['freq_data'])['asd_data'].quantile(upper_quantile)
+
+    # Evaluate the ratio between the quantities
+    diff_estim = first_estim - second_estim
+    diff_low_quantile = first_low_quantile - second_low_quantile
+    diff_up_quantile = first_up_quantile - second_up_quantile
+
+    # Frequencies are the indeces of the DataFrame because of groupby()
+    frequency_array = diff_estim.index.to_numpy()
+
+    # Convert the DataFrame in numpy array
+    diff_estim_array = diff_estim.to_numpy()
+    diff_low_quantile_array = diff_low_quantile.to_numpy()
+    diff_up_quantile_array = diff_up_quantile.to_numpy()
+
+    label_1 = first_df['Channels'][0].split(' ')[1].replace('(', '').replace(')', '')
+    label_2 = second_df['Channels'][0].split(' ')[1].replace('(', '').replace(')', '')
+    ratio_label = label_1 + ' - ' + label_2
+
+    return frequency_array, diff_estim_array, diff_low_quantile_array, diff_up_quantile_array, ratio_label
 
 
 df_1, t_w, f_samp = asd_extractor(
@@ -278,7 +438,7 @@ df_1, t_w, f_samp = asd_extractor(
     psd_window=60,
     means_number=10,
     channel='Sum')
-# print('first df')
+
 df_2, _, _ = asd_extractor(
     file_directory=r'C:\Users\lpese\PycharmProjects\Archimedes_repo\Characterization\Noise\1_kHz',
     filename=r'OL_Dark_GPZ_SoE_noUPS_Open_1kHz.lvm',
@@ -286,10 +446,26 @@ df_2, _, _ = asd_extractor(
     psd_window=60,
     means_number=10,
     channel='Sum')
-# print('second df')
-df_3 = ratio(first_df=df_1,
-             second_df=df_2)
-# print('third df')
+
+f_data, ratio_data, ratio_low, ratio_up, ratio_lab = ratio(first_df=df_1,
+                                                           second_df=df_2,
+                                                           lower_quantile=0.1,
+                                                           upper_quantile=0.9,
+                                                           estimator='median')
+
+# TO MAKE RATIO USING STANDARD DEVIATIONS
+# _, std_data, std_low, std_up, std_lab = ratio_std(first_df=df_1,
+#                                                   second_df=df_2,
+#                                                   num_std=2,
+#                                                   estimator='median')
+
+# TO MAKE DIFFERENCE BETWEEN SIGNALS
+# _, diff_data, diff_low, diff_up, diff_lab = difference(first_df=df_1,
+#                                                        second_df=df_2,
+#                                                        lower_quantile=0.1,
+#                                                        upper_quantile=0.9,
+#                                                        estimator='median')
+
 # MAKING PSD PLOT WITH 95% CONFIDENCE LEVEL
 fig_psd_confidence = plt.figure(figsize=(10, 5))
 
@@ -315,7 +491,18 @@ ax = gs.subplots(sharex=True)
 # See: https://seaborn.pydata.org/generated/seaborn.lineplot.html
 sns.lineplot(x="freq_data", y="asd_data", hue='Channels', palette=['tab:blue'], ci='sd', data=df_1, ax=ax[0])
 sns.lineplot(x="freq_data", y="asd_data", hue='Channels', palette=['tab:orange'], ci='sd', data=df_2, ax=ax[1])
-sns.lineplot(x="freq_data", y="ratio", hue='Channels', palette=['tab:green'], ci='sd', data=df_3, ax=ax[2])
+
+# PLOT RATIO
+ax[2].fill_between(f_data, ratio_low, ratio_up, alpha=.5, color='tab:green', linewidth=0)
+ax[2].plot(f_data, ratio_data, linewidth=2, color='tab:green', label=ratio_lab + ' (Median)')
+# ax[2].scatter(f_data, std_up, linewidth=1, color='tab:red', label='90%')
+# ax[2].scatter(f_data, std_low, linewidth=1, color='tab:blue', label='10%')
+
+# PLOT DIFFERENCE
+# ax[2].fill_between(f_data, diff_low, diff_up, alpha=.5, color='tab:green', linewidth=0)
+# ax[2].plot(f_data, diff_data, linewidth=2, color='tab:green', label=diff_lab)
+
+# sns.lineplot(x="freq_data", y="ratio", hue='Channels', palette=['tab:green'], ci='sd', data=df_3, ax=ax[2])
 
 # DEFINING VARIABLES TO CONTROL X/Y LIMIT IN PLOTS
 x_limit_inf = 1 / t_w  # minimum frequency achieved
@@ -348,5 +535,6 @@ ax[2].set_xlim([x_limit_inf, x_limit_sup])
 # ax3.set_xlim([y_limit_inf_ratio, y_limit_sup_ratio])
 ax[2].set_ylabel(r'$\frac{a}{b}$', fontsize=20)
 ax[2].grid(True, linestyle='--', axis='both', which='both')
+ax[2].legend(loc='best', shadow=True, fontsize='medium')
 
 plt.show()
