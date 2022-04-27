59afd7ff50aeb263279b26f506f1659ef09919d6
Arc_MDC <archimedes.noreply@gmail.com>
2022-04-27 18:56:12 +0200

    Modified PI-motor GUI code

--- PIpython/GUI_PI.py
+++ PIpython/GUI_PI.py
@@ -8,29 +8,41 @@ import subprocess
 import webbrowser
 import numpy as np
 import sys
+
 sys.dont_write_bytecode = True
 
-import PI_functions # import the Analysis module
+import PI_functions  # import the Analysis module
+
 
 # Create GUI
 def create_window():
     window = tk.Tk()
+
+
 # do Nothing
 def doNothing():
     print("nothing")
 
+
 def import_M1():
     global M1
     m1 = bool(entry.get())
+    print(m1)
     M1.set(m1)
+
+
 def import_M2():
     global M2
     m2 = bool(entry.get())
     M2.set(m2)
+
+
 def import_VEL():
     global Vel
     v = bool(entry.get())
     Vel.set(v)
+
+
 # def import_StepSize():
 #     global StepSize
 #     ss = bool(entry.get())
@@ -41,14 +53,33 @@ def import_NAxis():
     naxis = int(entry.get())
     NAxis.set(naxis)
 
+
 def ReadAxis():
     global axis0
-    axis0 = PI_functions.fz_ReadAxis(bool(M1.get()),bool(M2.get()),int(Axis.get()))
+    axis0 = PI_functions.fz_ReadAxis(bool(M1.get()), bool(M2.get()), int(Axis.get()))
     Axis0.set(axis0)
+
+
 def MoveAxis():
     global axis1
-    axis1 = PI_functions.fz_MoveAxis(bool(M1.get()),bool(M2.get()),int(Axis.get()),float(Targ.get()),float(Vel.get()))#,float(StepSize.get()))
+    axis1 = PI_functions.fz_MoveAxis(bool(M1.get()), bool(M2.get()), int(Axis.get()), float(Targ.get()),
+                                     float(Vel.get()))
     Axis1.set(axis1)
+
+
+def MoveAxisOPEN():
+    global axis1OPEN
+    axis1OPEN = PI_functions.fz_MoveAxis(bool(M1.get()), bool(M2.get()), int(Axis.get()), float(TargOPEN.get()),
+                                         float(Vel.get()))
+    Axis1OPEN.set(axis1OPEN)
+
+
+def MoveAxisCLOSE():
+    global axis1CLOSE
+    axis1CLOSE = PI_functions.fz_MoveAxis(bool(M1.get()), bool(M2.get()), int(Axis.get()), float(TargCLOSE.get()),
+                                          float(Vel.get()))
+    Axis1CLOSE.set(axis1CLOSE)
+
 
 root = tk.Tk()
 root.title("Physical Instrument Python GUI for Archimedes")
@@ -64,10 +95,7 @@ Checkbutton(frameStart, text="E-872 (2 axis)", variable=M2).grid(row=1, column=0
 
 tk.Label(frameStart, text='Velocity').grid(row=2, column=0)
 Vel = tk.StringVar(value=0.5)
-Vel_ent = tk.Entry(frameStart, textvariable=Vel).grid(row=2, column=1)
-# tk.Label(frameStart, text='Stepsize').grid(row=3, column=0)
-# StepSize = tk.StringVar(value=0.01)
-# ss_ent = tk.Entry(frameStart, textvariable=StepSize).grid(row=3, column=1)
+tk.Entry(frameStart, textvariable=Vel).grid(row=2, column=1)
 
 ########################################################################################
 frame1 = tk.LabelFrame(root, text='Read and Write', relief=tk.GROOVE)
@@ -75,35 +103,49 @@ frame1.grid(row=0, column=1, padx=10, pady=2)
 
 tk.Label(frame1, text='Axis').grid(row=0, column=0)
 Axis = tk.StringVar(value=1)
-Axis_ent = tk.Entry(frame1, textvariable=Axis).grid(row=0, column=1)
+tk.Entry(frame1, textvariable=Axis).grid(row=0, column=1)
 tk.Button(frame1, text='Read axis value', command=ReadAxis).grid(row=0, column=2)
 Axis0 = tk.StringVar()
-axis0_ent = tk.Entry(frame1, textvariable=Axis0).grid(row=0, column=3)
+tk.Entry(frame1, textvariable=Axis0).grid(row=0, column=3)
 
 tk.Label(frame1, text='Set value').grid(row=1, column=0)
 Targ = tk.StringVar(value=5.)
-Targ_ent = tk.Entry(frame1, textvariable=Targ).grid(row=1, column=1)
+tk.Entry(frame1, textvariable=Targ).grid(row=1, column=1)
 tk.Button(frame1, text='Move to target value', command=MoveAxis).grid(row=1, column=2)
 Axis1 = tk.StringVar()
-axis1_ent = tk.Entry(frame1, textvariable=Axis1).grid(row=1, column=3)
+tk.Entry(frame1, textvariable=Axis1).grid(row=1, column=3)
+
+tk.Label(frame1, text='Set OPEN value').grid(row=2, column=0)
+TargOPEN = tk.StringVar(value=2.)
+tk.Entry(frame1, textvariable=TargOPEN).grid(row=2, column=1)
+tk.Button(frame1, text='Move to OPEN target value', command=MoveAxisOPEN).grid(row=2, column=2)
+Axis1OPEN = tk.StringVar()
+tk.Entry(frame1, textvariable=Axis1OPEN).grid(row=2, column=3)
+
+tk.Label(frame1, text='Set CLOSE value').grid(row=3, column=0)
+TargCLOSE = tk.StringVar(value=7.)
+tk.Entry(frame1, textvariable=TargCLOSE).grid(row=3, column=1)
+tk.Button(frame1, text='Move to OPEN target value', command=MoveAxisCLOSE).grid(row=3, column=2)
+Axis1CLOSE = tk.StringVar()
+tk.Entry(frame1, textvariable=Axis1CLOSE).grid(row=3, column=3)
 
 ########################################################################################
 ########################################################################################
 # Close
 frameEnd = tk.LabelFrame(root, text='Quit program', relief=tk.GROOVE)
 frameEnd.grid(row=3, column=0, padx=10, pady=2)
-tk.Button(frameEnd, text='Close',command=root.destroy).grid(row=0, column=1)
+tk.Button(frameEnd, text='Close', command=root.destroy).grid(row=0, column=1)
 
-menu =  Menu(root)
+menu = Menu(root)
 root.config(menu=menu)
 subMenu = Menu(menu)
-menu.add_cascade(label="File",menu=subMenu)
+menu.add_cascade(label="File", menu=subMenu)
 subMenu.add_command(label="New", command=create_window)
 subMenu.add_command(label="Open", command=doNothing)
 subMenu.add_command(label="Restart", command=doNothing)
 subMenu.add_command(label="Exit", command=doNothing)
 editMenu = Menu(menu)
-menu.add_cascade(label = "Help", menu=editMenu)
+menu.add_cascade(label="Help", menu=editMenu)
 editMenu.add_command(label="Help", command=doNothing)
 
 root.geometry('+300+250')
--- PIpython/PI_functions.py
+++ PIpython/PI_functions.py
@@ -11,6 +11,7 @@ import scipy.fftpack
 import csv, time, datetime
 from pipython import GCSDevice, pitools
 
+
 def fz_Motor(M1, M2):
     """
     Read axis
@@ -29,25 +30,37 @@ def fz_Motor(M1, M2):
     -------
     which motor: CONTROLLERNAME,STAGES,REFMODE,SN
     """
-
+    # REFMODE:
+    # FNL: Start a reference move to the negative limit switch.
+    #      Moves all 'axes' synchronously to the negative physical limits
+    #      of their travel ranges and sets the current positions to the negative
+    #      range limit values
+    # FPL: Start a reference move to the positive limit switch.
+    #      Moves all 'axes' synchronously to the positive physical limits
+    #      of their travel ranges and sets the current positions to the positive
+    #      range limit values.
+    # FRF: Start a reference move to the reference switch. (half range)
+    #      Moves all 'axes' synchronously to the physical reference point
+    #      and sets the current positions to the reference position.
     CONTROLLERNAME = 'C-663.12'
     STAGES = ('M-228.10S')  # connect stages to axes
-    REFMODE = ('FNL')  # reference the connected stages
+    REFMODE = ('')  # reference the connected stages
     SN = '021550465'
     if M1 == True and M2 == False:
-       CONTROLLERNAME = 'C-663.12'
-       STAGES = ('M-228.10S')  # connect stages to axes
-       REFMODE = ('FNL')  # reference the connected stages
-       SN = '021550465'
+        CONTROLLERNAME = 'C-663.12'
+        STAGES = ('M-228.10S')  # connect stages to axes
+        REFMODE = ('')  # reference the connected stages
+        SN = '021550465'  # 021550465 SN stage in UNISS
     elif M1 == False and M2 == True:
-       CONTROLLERNAME = 'E-872.401'
-       STAGES = ('N-480K111')  # connect stages to axes
-       REFMODE = ('FNL')  # reference the connected stages
-       SN = '021550465'
+        CONTROLLERNAME = 'E-872.401'
+        STAGES = ('N-480K111')  # connect stages to axes
+        REFMODE = ('')  # reference the connected stages
+        SN = '021550465'
     else:
-       print('Choose only one motor')
+        print('Choose only one motor')
 
-    return CONTROLLERNAME,STAGES,REFMODE,SN
+    return CONTROLLERNAME, STAGES, REFMODE, SN
+
 
 def fz_ReadAxis(M1, M2, Axis):
     """
@@ -70,37 +83,22 @@ def fz_ReadAxis(M1, M2, Axis):
     axis position
     """
 
-#    CONTROLLERNAME = 'C-663.12'
-#    STAGES = ('M-228.10S')  # connect stages to axes
-#    REFMODE = ('FNL')  # reference the connected stages
-#    SN = '021550465'
-#    if M1 == True and M2 == False:
-#       CONTROLLERNAME = 'C-663.12'
-#       STAGES = ('M-228.10S')  # connect stages to axes
-#       REFMODE = ('FNL')  # reference the connected stages
-#       SN = '021550465'
-#    elif M1 == False and M2 == True:
-#       CONTROLLERNAME = 'C-663.12'
-#       STAGES = ('M-228.10S')  # connect stages to axes
-#       REFMODE = ('FNL')  # reference the connected stages
-#    else:
-#       print('Choose only one motor')
     CONTROLLERNAME, STAGES, REFMODE, SN = fz_Motor(M1, M2)
-    print(CONTROLLERNAME,STAGES,REFMODE,SN)
+    print(CONTROLLERNAME, STAGES, REFMODE, SN)
     with GCSDevice(CONTROLLERNAME) as pidevice:
         pidevice.ConnectUSB(serialnum=SN)
-        #pidevice.InterfaceSetupDlg(key='sample')
+        # pidevice.InterfaceSetupDlg(key='sample')
         print('initialize connected stages...')
-        pitools.startup(pidevice, stages=STAGES, refmodes=REFMODE, servostates=True)
-        print('after startup')
-        positions = pidevice.qPOS(pidevice.axes)
-        #print('position of axis {} = {:.2f}'.format(Axis, positions[Axis]))
-        for Axis in pidevice.axes:
-            print('position of axis {} = {:.2f}'.format(Axis, positions[Axis]))
+        pitools.startup(pidevice, stages=STAGES, refmodes=None, servostates=True)
+        positions = pidevice.qPOS(Axis)
+        print('position of axis', Axis, '=', positions[Axis])
+        # for Axis in pidevice.axes:
+        #     print('position of axis {} = {:.2f}'.format(Axis, positions[Axis]))
 
     return positions[Axis]
 
-def fz_MoveAxis(M1, M2, Axis, target, Vel):#, StepSize):
+
+def fz_MoveAxis(M1, M2, Axis, target, Vel):
     """
     Move axis
 
@@ -128,20 +126,26 @@ def fz_MoveAxis(M1, M2, Axis, target, Vel):#, StepSize):
     """
 
     CONTROLLERNAME, STAGES, REFMODE, SN = fz_Motor(M1, M2)
-    print(CONTROLLERNAME,STAGES,REFMODE,SN)
+    print(CONTROLLERNAME, STAGES, REFMODE, SN)
     with GCSDevice(CONTROLLERNAME) as pidevice:
         pidevice.ConnectUSB(serialnum=SN)
         print('initialize connected stages...')
-        pitools.startup(pidevice, stages=STAGES, refmodes=REFMODE, servostates=True)
-        print('after startup')
-        pidevice.VEL(pidevice.axes, Vel)
-        #pidevice.SST(pidevice.axes, StepSize)
-        print('move stages...')
-        pidevice.MOV(pidevice.axes, target)
-        pitools.waitontarget(pidevice)
-
-        positions = pidevice.qPOS(pidevice.axes)
-        for Axis in pidevice.axes:
-            print('position of axis {} = {:.2f}'.format(Axis, positions[Axis]))
+        pitools.startup(pidevice, stages=STAGES, refmodes='FRF', servostates=True)
+        pidevice.VEL(Axis, Vel)
+        rangemin = list(pidevice.qTMN(Axis).values())
+        rangemax = list(pidevice.qTMX(Axis).values())
+        print(rangemin[0], rangemax[0])
+        if target < rangemax[0] and target > rangemin[0]:
+            print('move stages...')
+            pidevice.MOV(Axis, target)
+            # pidevice.MVR(Axis, target)
+            pitools.waitontarget(pidevice)
+        else:
+            print('target value out of range', rangemin[0], '-', rangemax[0])
+        positions = pidevice.qPOS(Axis)
+        print('position of axis', Axis, '=', positions[Axis])
+        # pidevice.POS(Axis, positions)
+        # for Axis in pidevice.axes:
+        #    print('position of axis {} = {:.2f}'.format(Axis, positions[Axis]))
 
     return positions[Axis]
