5bd6e0708efd50d054d152a6d3edfeb9a8fa0702
Arc_MDC <archimedes.noreply@gmail.com>
2022-07-11 12:12:14 +0200

    Updates from lab machine

--- ET_sensors/ET_RMS_TimeEvolution.py
+++ ET_sensors/ET_RMS_TimeEvolution.py
@@ -23,36 +23,42 @@ import ET_common as Ec
 # TODO: add sensibility of the seismometer (e.g. x_lim = 1/240s)
 
 config = configparser.ConfigParser()
-config.read('Time_Evolution.ini')
+config.read('RMS_time_config.ini')
 
 # TODO: add check for file already existing
 df_path = config['Paths']['outDF_path']  # TODO: change outDF_path name in a more right one
 i_min = float(config['Quantities']['integral_min'])
 i_max = float(config['Quantities']['integral_max'])
+out_error = config.getboolean('DEFAULT', 'save_error')
 freq_df_path = Ec.check_dir(df_path, 'Freq_df')
 npz_path = Ec.check_dir(df_path, 'npz_files')
 daily_path = Ec.check_dir(df_path, 'daily_df')
 t_log = time.strftime('%Y%m%d_%H%M')
 f_len = 0
 
-Twindow = int(config['Quantities']['psd_window'])  # TODO: remove this line and make it automatically check on filename
+# Twindow = int(config['Quantities']['psd_window'])  # TODO: remove this line and make it automatically check on filename
 
 
-def eval_rms(filename, freq_len, psd_len, chunk_index):
-    global i_min, i_max
+def eval_rms(filename, freq_len, psd_len, Twindow, chunk_index):
+    # global i_min, i_max
     temp_df = pd.read_parquet(filename)
     day_num = filename.replace(daily_path + '\\', '').split('.')[0].split('_')[1].split('-')[1]
     year = filename.replace(daily_path + '\\', '').split('.')[0].split('_')[1].split('-')[0]
     # evaluate the integral in the frequency region between i_min and i_max
     rms = temp_df.iloc[chunk_index * freq_len:chunk_index * freq_len + freq_len].loc[i_min:i_max].sum()[0] * freq_len
     date = datetime.datetime.strptime(year + "-" + day_num, "%Y-%j") + datetime.timedelta(seconds=psd_len * chunk_index)
+    if out_error and rms == 0:
+        print(temp_df.iloc[chunk_index * freq_len:chunk_index * freq_len + freq_len].loc[i_min:i_max])
+        with open(df_path + fr'\RMS-Error_{t_log}.txt', 'a') as text_file:
+            print(f'{filename}|{date}|{Twindow}', file=text_file)
     # date = date.timestamp()
     return rms, date
 
 
 def to_rms():
-    global daily_path, freq_df_path, npz_path, f_len, Twindow
+    # global daily_path, freq_df_path, npz_path, f_len  # , Twindow
     filename_list = glob.glob(daily_path + r"\*.brotli")
+    Twindow = filename_list[0].split('_')[2][-4:]  # TODO: find a better way
     data = np.load(npz_path + fr'\{Twindow}_Frequency.npz')
     freq_data = data['frequency']
     f_len = len(freq_data)
@@ -65,7 +71,7 @@ def to_rms():
         for f_index, file in enumerate(filename_list):
             j1 = (f_index + 1) / len(filename_list)
             print("\r[%-75s] %g%%" % ('=' * int(75 * j1), round(100 * j1, 3)), end='\n')
-            results = executor.map(functools.partial(eval_rms, file, f_len, psd_length), num_asd)
+            results = executor.map(functools.partial(eval_rms, file, f_len, psd_length, Twindow), num_asd)
             for res in results:
                 rms_date_lst.append(res)
     return rms_date_lst
